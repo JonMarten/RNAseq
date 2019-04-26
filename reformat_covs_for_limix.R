d4ac3fa980554e06c808e862566a5d7f5007a446
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-04-26 12:34:52 +0100

    Finish RNA id mapper script.

--- reformat_covs_for_limix.R
+++ reformat_covs_for_limix.R
@@ -10,21 +10,48 @@ ids[which(ids == "", arr.ind=T)] <- NA
 allids <- full_join(ids, ids3)
 dat2 <- full_join(allids, dat)
 
-# Filter to only individuals with RNA at either 24 and/or 48 months
-ids_RNA <- ids %>% 
-  filter(!is.na(RNAseq_QC_24m) | !is.na(RNAseq_QC_48m)) %>%
-  mutate(id_RNA = ifelse(is.na(RNAseq_QC_24m), RNAseq_RAW_24m, RNAseq_QC_48m)) %>%
-  select(identifier, Affymetrix_QC_bl, id_RNA)
- 
-dat_RNA <- left_join(ids_RNA, dat)
-
-#map <- fread("v01_188id_preview/sample_mapping_file_gt_to_phe.txt", data.table=F)
-
+# Read in ids from RNA seq data
 rna_seq_ids <- fread("globus/RNA_seq_ids.csv", data.table = F)
 
-a <- apply(dat2, MARGIN = 2, FUN = function(x){names(dat2)[which(map$phenotype_individual_id %in% x)]}) 
-
+# filter to just RNA id columns and rows with an RNA id
 RNAcols <- grep("RNA", names(allids))
-for(i in RNAcols){
-  which(rna_seq_ids$RNA_id %in% allids[,i]) %>% length %>% print
+RNArows <- rowSums(is.na(rnaids[,-1])) != ncol(rnaids[,-1]) # list rows with at least one non-NA RNA id
+rnaids <- allids %>% 
+  select(identifier, RNAcols) %>%
+  filter(RNArows) %>%
+  mutate(RNAseq_gwasQC_24m = as.character(RNAseq_gwasQC_24m),
+         RNAseq_gwasQC_48m = as.character(RNAseq_gwasQC_48m),
+         RNAseq_gwasQC_p3 = as.character(RNAseq_gwasQC_p3))
+
+# Make single column for RNA identifier
+RNA_any <- NA
+for(i in 1:nrow(rnaids)){
+  ids <- as.character(rnaids[i,-1])
+  RNA_any[i] <- paste(unique(na.exclude(ids)), collapse = ",")
 }
+rnaids$RNA_any <- RNA_any
+
+# Check overlap with RNA seq data identifiers
+rnamiss <- which(!rna_seq_ids$RNA_id %in% rnaids$RNA_any)
+phemiss <-which(!rnaids$RNA_any %in% rna_seq_ids$RNA_id )
+
+if(length(rnamiss) == 0){
+  cat("\n No RNA seq ids missing in phenotype data.")
+} else {
+  cat("\n",
+      length(rnamiss),
+      " RNA seq ids missing in phenotype data:", 
+      paste0(rna_seq_ids$RNA_id[rnamiss]))
+}
+
+if(length(phemiss) == 0){
+  cat("\n No phenotype data ids missing in RNA seq.")
+} else {
+  cat("\n",
+      length(phemiss),
+      " phenotype data ids missing in RNA seq:", 
+      paste0(rnaids$RNA_any[phemiss], collapse = ", "))
+}
+
+rna_id_mapper <- rnaids %>%
+  select(identifier, RNA_any)
