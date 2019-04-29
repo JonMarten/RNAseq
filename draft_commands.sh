4ccb81b201ed4a33f9051548d434342586b14846
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-04-29 16:28:33 +0100

    Changes to draft commands file

--- draft_commands.sh
+++ draft_commands.sh
@@ -64,36 +64,15 @@ python /home/jm2294/projects/RNAseq/hipsci_pipeline/limix_QTL_pipeline/run_QTL_a
  -af ${PHEPATH}/Feature_Annotation_Ensembl_gene_ids_autosomes.txt\
  -pf ${PHEPATH}/phenotype_5281-fc-genecounts.txt\
  -od ${OUTPATH}\
- -c\
  --sample_mapping_file ${PHEPATH}/sample_mapping_file_gt_to_phe.txt\
- #-cf ${PHEPATH}/Geuvadis_CEU_YRI_covariates.txt\
- #--kinship_file ${GENPATH}/Geuvadis_chr1_kinship.normalized.txt\
+ -t\
  -np 10000\
  -maf 0.000000001\
  -hwe 0.00001\
  -cr 0.95\
  -gm standardize\
  -w 250000\
- --block_size 1500\
- 
-source activate limix_qtl
-GENPATH=/home/jm2294/GENETIC_DATA/INTERVAL/master
-PHEPATH=/home/jm2294/projects/RNAseq/test_run
-OUTPATH=/home/jm2294/projects/RNAseq/test_run/output
-	
-python /home/jm2294/projects/RNAseq/hipsci_pipeline/limix_QTL_pipeline/run_QTL_analysis.py\
- --bgen /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile\
- -af ${PHEPATH}/Feature_Annotation_Ensembl_gene_ids_autosomes.txt\
- -pf ${PHEPATH}/phenotype_5281-fc-genecounts.txt\
- -od ${OUTPATH}\
- --sample_mapping_file ${PHEPATH}/sample_mapping_file_gt_to_phe.txt\
- -c\
- -np 10000\
- -maf 0.000000001\
- -hwe 0.00001\
- -cr 0.95\
- -gm standardize\
- -w 250000\
- --block_size 1500\
+ --block_size 50\
+ #-c\
  #-cf ${PHEPATH}/Geuvadis_CEU_YRI_covariates.txt\
  #--kinship_file ${GENPATH}/Geuvadis_chr1_kinship.normalized.txt\
