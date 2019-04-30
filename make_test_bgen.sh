49b2fc357d6d5683ebca1714943835fc38f01c2f
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-04-30 16:11:49 +0100

    Rename script

---
+++ make_test_bgen.sh
@@ -0,0 +1,28 @@
+
+module load gcc/5.2.0 
+module load qctool2/rc4-6.8
+qctool\
+ -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen\
+ -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ -incl-samples /home/jm2294/projects/RNAseq/test_run/batch1_ids.txt\
+ -incl-range 22:23500000-24500000\
+ -og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile.bgen
+ 
+ qctool\
+ -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen\
+ -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ -incl-samples /home/jm2294/projects/RNAseq/test_run/batch1_ids.txt\
+ -incl-range 22:23500000-24500000\
+ -excl-snpids /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/filter_snps.txt\
+ -og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids.bgen
+ 
+ # Filter out SNPs with no unique identifier ## this loses sample info, have to remake from scratch
+ #qctool\
+ #-g /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile.bgen\
+ #-excl-snpids filter_snps.txt\
+ #-og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids.bgen
+ 
+ 
+ # check snp lists
+qctool -g /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile.bgen -snp-stats -osnp test_bgen_snp_stats.txt
+qctool -g /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids.bgen -snp-stats -osnp test_bgen_snp_stats_unique.txt
