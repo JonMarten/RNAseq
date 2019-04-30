72ab6c72abe48781fedd3cb5bb741ecf00054308
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-04-30 15:53:34 +0100

    Make script to pull out duplicate rsids in bgen file.

---
+++ get_duplicate_rsids.R
@@ -0,0 +1,20 @@
+# get list of duplicated SNPs to filter out
+
+library(data.table)
+library(dplyr)
+a <- fread("test_bgen_snp_stats_unique.txt", data.table=F, skip = "#")
+a <- read.table("test_bgen_snp_stats.txt", h=T, stringsAsFactors = F)
+b <- read.table("test_bgen_snp_stats_unique.txt", h=T, stringsAsFactors = F)
+
+a2 <- a %>% 
+  group_by(rsid) %>% 
+  filter(n() > 1) %>% 
+  data.frame
+
+b2 <- b %>% 
+  group_by(rsid) %>% 
+  filter(n() > 1) %>% 
+  data.frame
+
+dupes <- data.frame(unique(a2$alternate_ids))
+write.table(dupes, row.names = F, col.names = F, quote = F, file = "filter_snps.txt")
