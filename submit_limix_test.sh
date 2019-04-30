ce202d06041ac216fa4ea0241dc9f38903e525f3
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-04-30 16:53:30 +0100

    Update to use bgen without duplicates.

--- submit_limix_test.sh
+++ submit_limix_test.sh
@@ -13,12 +13,12 @@ PHEPATH=/home/jm2294/projects/RNAseq/test_run
 OUTPATH=/home/jm2294/projects/RNAseq/test_run/output
 
 python /home/jm2294/projects/RNAseq/hipsci_pipeline/limix_QTL_pipeline/run_QTL_analysis.py\
- --bgen ${GENPATH}/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile\
+ --bgen ${GENPATH}/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids\
  -af ${PHEPATH}/Feature_Annotation_Ensembl_gene_ids_autosomes.txt\
  -pf ${PHEPATH}/phenotype_5281-fc-genecounts.txt\
  -od ${OUTPATH}\
  --sample_mapping_file ${PHEPATH}/sample_mapping_file_gt_to_phe.txt\
- -t\
+ -c\
  -np 10000\
  -maf 0.000000001\
  -hwe 0.00001\
