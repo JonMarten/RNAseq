5ea1088f7bae9a382dfda39e3fb9a0244d965f4f
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-09 15:18:36 +0100

    Alter script to run on entire chromosome 22.

--- get_duplicate_rsids.R
+++ get_duplicate_rsids.R
@@ -1,18 +1,19 @@
 # get list of duplicated SNPs to filter out
-
+setwd("/home/jm2294/GENETIC_DATA/INTERVAL/RNAseq")
 library(dplyr)
-a <- read.table("test_bgen_snp_stats.txt", h=T, stringsAsFactors = F)
-b <- read.table("test_bgen_snp_stats_unique.txt", h=T, stringsAsFactors = F)
+a <- read.table("c22_test_bgen_snp_stats.txt", h=T, stringsAsFactors = F)
 
 a2 <- a %>% 
   group_by(rsid) %>% 
   filter(n() > 1) %>% 
   data.frame
 
+dupes <- data.frame(unique(a2$alternate_ids))
+write.table(dupes, row.names = F, col.names = F, quote = F, file = "c22_filter_snps.txt")
+
+#check for duplicates after making new file
+b <- read.table("test_bgen_snp_stats_unique.txt", h=T, stringsAsFactors = F)
 b2 <- b %>% 
   group_by(rsid) %>% 
   filter(n() > 1) %>% 
   data.frame
-
-dupes <- data.frame(unique(a2$alternate_ids))
-write.table(dupes, row.names = F, col.names = F, quote = F, file = "filter_snps.txt")
--- make_test_bgen.sh
+++ make_test_bgen.sh
@@ -23,6 +23,30 @@ qctool\
  #-og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids.bgen
  
  
- # check snp lists
+# check snp lists
 qctool -g /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile.bgen -snp-stats -osnp test_bgen_snp_stats.txt
 qctool -g /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids.bgen -snp-stats -osnp test_bgen_snp_stats_unique.txt
+
+# Make new test bgen for whole c22
+# Get SNP Stats
+module load gcc/5.2.0 
+module load qctool2/rc4-6.8
+qctool\
+ -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen\
+ -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ -incl-samples /home/jm2294/projects/RNAseq/test_run/batch1_ids.txt\
+ -snp-stats\
+ -osnp c22_test_bgen_snp_stats.txt
+qctool\
+ -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen\
+ -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ -incl-samples /home/jm2294/projects/RNAseq/test_run/batch1_ids.txt\
+ -og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples_testfile.bgen
+
+qctool\
+ -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen\
+ -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ -incl-samples /home/jm2294/projects/RNAseq/test_run/batch1_ids.txt\
+ -incl-range 22:23500000-24500000\
+ -excl-snpids /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/c22_filter_snps.txt\
+ -og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/impute_22_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids.bgen
