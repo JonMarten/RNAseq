b0a77a01f54990d59f001b3f1e9732cc007d7c2c
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-16 16:27:24 +0100

    Complete script

--- make_small_phenotype_file.r
+++ make_small_phenotype_file.r
@@ -6,4 +6,8 @@ ann2 <- ann %>%
   arrange(chromosome, start) %>%
   filter(chromosome == 22, start > 23500000, end < 24500000)
 
-phe <- fread("phenotype_5281-fc-genecounts.txt", data.table=T)
+phe <- fread("phenotype_5281-fc-genecounts.txt", data.table=F)
+phe2 <- phe %>%
+  filter(ENSEMBL_ID %in% ann2$ensembl_gene_id)
+
+fwrite(phe2, sep = "\t", file = "phenotype_5281-fc-genecounts_chr22_23500000-24500000.txt")
