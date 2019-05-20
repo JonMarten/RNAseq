e4988ab1c4b1f3c2ca428e84368ef9c513c32f73
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-20 17:04:08 +0100

    Update for data release 13MAY2019

--- reformat_covs_for_limix.R
+++ reformat_covs_for_limix.R
@@ -3,9 +3,11 @@ library(dplyr)
 library(data.table)
 setwd("U:/Projects/RNAseq")
 
-dat <- fread("covariates/INTERVALdata_02APR2019.csv", data.table=F)
+dat <- fread("covariates/sarah_data_2/INTERVALdata_13MAY2019.csv", data.table=F)
+datp3 <- fread("covariates/sarah_data_2/INTERVALdata_P3_13MAY2019.csv", data.table=F)
 rna_id_mapper <- fread("rna_id_mapper.csv", data.table=F)
 
+dat <- full_join(dat, datp3, by="identifier")
 dat <- left_join(rna_id_mapper, dat)
 dat <- dat %>%
   select(sample_id = RNA_id, sexPulse, agePulse)
