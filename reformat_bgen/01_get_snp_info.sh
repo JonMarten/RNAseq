b4851fb8bcb15378fb46cee555a007f555dba3ec
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-29 11:29:07 +0100

    Loop over INTERVAL master bgen files to get SNP stats.

---
+++ reformat_bgen/01_get_snp_info.sh
@@ -0,0 +1,12 @@
+# Get SNP info from master bgen files
+module load gcc/5.2.0 
+module load qctool2/rc4-6.8
+
+for CHR in 1 .. 22
+do
+  qctool\
+   -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_${CHR}_interval.bgen\
+   -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+   -snp-stats\
+   -osnp /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/snp_stats/impute_${CHR}_interval_snp_stats_unfiltered.txt
+done
