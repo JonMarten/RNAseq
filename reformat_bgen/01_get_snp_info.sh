9759b1c57fc5d00a026db6a287d6ddd8b83e91d7
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-29 12:12:35 +0100

    Convert to array job

--- reformat_bgen/01_get_snp_info.sh
+++ reformat_bgen/01_get_snp_info.sh
@@ -3,11 +3,8 @@
 module load gcc/5.2.0 
 module load qctool2/rc4-6.8
 
-for CHR in 1 .. 22
-do
-  qctool\
-   -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_${CHR}_interval.bgen\
+qctool\
+   -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_${SLURM_ARRAY_TASK_ID}_interval.bgen\
    -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
    -snp-stats\
-   -osnp /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/snp_stats/impute_${CHR}_interval_snp_stats_unfiltered.txt
-done
+   -osnp /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/snp_stats/impute_${SLURM_ARRAY_TASK_ID}_interval_snp_stats_unfiltered.txt
