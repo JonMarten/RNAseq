b105b8280800d90b08b8ff327b30a463d388f1eb
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-31 16:58:32 +0100

    Add code to deal with duplicates in b38 information. NOTE THAT b38 INFO IS STILL INCOMPLETE.

--- reformat_bgen/02_generate_snp_ids.R
+++ reformat_bgen/02_generate_snp_ids.R
@@ -18,6 +18,7 @@ snpstats <- snpstats %>%
 b38 <- fread(paste0("/home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/b37_b38_liftover/INTERVAL_24_8_18_imputed_chr",chr,"_hg38.vcf"), data.table = F, skip = 5)
 names(b38) <- c("CHROM.b38","POS.b38","rsid","REF.b38","ALT.b38")
 b38 <- b38 %>%
+  distinct %>%
   filter(rsid != ".") %>%
   mutate(REF.b38_sort = ifelse(REF.b38 < ALT.b38, REF.b38, ALT.b38),
          ALT.b38_sort = ifelse(REF.b38 < ALT.b38, ALT.b38, REF.b38))  %>%
@@ -29,9 +30,6 @@ no_b38 <- snpstats$match_id[which(!snpstats$match_id %in% snpsmerge$match_id)]
 # Create CPTIDs for SNPs (sort alleles in alphabetical order)
 snps <- snpsmerge %>%
   filter(indel == 0) %>%
-  filter(!is.na(CHROM.b38)) %>%
-  mutate(REF.b38_sort = ifelse(REF.b38 < ALT.b38, REF.b38, ALT.b38),
-         ALT.b38_sort = ifelse(REF.b38 < ALT.b38, ALT.b38, REF.b38)) %>%
   mutate(cptid.b38 = paste0(CHROM.b38, ":", POS.b38, "_", REF.b38_sort, "_", ALT.b38_sort))
 
 # Create CPTIDs for indels (sort alleles by smallest first)
@@ -42,30 +40,18 @@ indels <- snpsmerge %>%
          ALT.b38_sort = ifelse(nchar(REF.b38) > nchar(ALT.b38), REF.b38, ALT.b38)) %>%
   mutate(cptid.b38 = paste0(CHROM.b38, ":", POS.b38, "_", REF.b38_sort, "_", ALT.b38_sort))
 
-# Check for duplicated ids on SNPs
-snpdupe <- snps %>% 
-  group_by(cptid.b38) %>%
-  filter(n() > 1) %>%
-  data.frame()%>%
-  mutate(alleleA_sort = ifelse(alleleA <= alleleB, alleleA, alleleB),
-         alleleB_sort = ifelse(alleleA <= alleleB, alleleB, alleleA)) %>%
-  mutate(cptid = paste0(CHROM.b38, ":", POS.b38, "_", alleleA_sort, "_", alleleB_sort))
-
-# Check for duplicated ids on indels
-dupes <- indels %>% 
-  group_by(cptid) %>%
-  filter(n() > 1) %>%
-  data.frame()
-
-# Append rsid to indels with duplicated cptids
-if(nrow(dupes > 0)){
-  dupevec <- which(indels$cptid %in% dupes$cptid)
-  indels$cptid[dupevec] <- paste0(indels$cptid[dupevec], "___",indels$rsid[dupevec])
-}
-
 # Merge SNPs and indels back together
 b <- rbind(snps, indels) %>%
   arrange(chromosome, position)
+
+dupes <- b %>% 
+  group_by(cptid.b38) %>%
+  filter(n() > 1) %>%
+  data.frame()
+if(nrow(dupes > 0)){
+  dupevec <- which(b$cptid.b38 %in% dupes$cptid.b38)
+  b$cptid.b38[dupevec] <- paste0(b$cptid.b38[dupevec], "___",b$rsid.x[dupevec])
+}
 
 
 
