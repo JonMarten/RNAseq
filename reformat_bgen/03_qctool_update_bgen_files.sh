66a9af7afae4675b492b476d7ab7ce3e076ed9f1
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-05-31 17:15:56 +0100

    Bash script to update bgen files

---
+++ reformat_bgen/03_qctool_update_bgen_files.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Get SNP info from master bgen files
+#SBATCH -p long
+#SBATCH --job-name=snpstats
+#SBATCH -t 36:0:0
+
+# get start time
+start=$(date +%s.%N)
+
+module load gcc/5.2.0 
+module load qctool2/rc4-6.8
+
+qctool\
+ -g /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_${SLURM_ARRAY_TASK_ID}_interval.bgen\
+ -s /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ -map-id-data\
+ -og /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/b37_b38_liftover/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
+ 
+ 
+end=$(date +%s.%N)    
+runtime=$(python -c "print(${end} - ${start})")
+
+echo "Runtime was $runtime"
