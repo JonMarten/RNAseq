59020aa525bddc80be1624ecbd3c9c0a7527683c
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-06-25 15:27:03 +0100

    Parallelise

--- reformat_bgen/plink_get_snp_info.sh
+++ reformat_bgen/plink_get_snp_info.sh
@@ -3,7 +3,7 @@ module load plink/2.0_09_09_18
 
 cd /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/b37_b38_liftover
 
-for i in {1..22}
-do
-  plink2 --sample /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample --bgen /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_${i}_interval.bgen --out /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/b37_b38_liftover/plink/impute_${i}_interval_b38_filtered_on_rsids --write-snplist
-done
+plink2 \
+ --sample /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_22_interval.bgen.sample\
+ --bgen /home/jm2294/GENETIC_DATA/INTERVAL/master/impute_${SLURM_ARRAY_TASK_ID}_interval.bgen\
+ --out /home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/plink/impute_${SLURM_ARRAY_TASK_ID}_b37_unfiltered
