20212ea4a8c48f9784377db5bc196170bbd14e2d
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-06-27 16:04:54 +0100

    Manual array specification

--- reformat_bgen/02b_submit_snp_ids.sh
+++ reformat_bgen/02b_submit_snp_ids.sh
@@ -5,7 +5,6 @@
 #SBATCH --output=/home/jm2294/GENETIC_DATA/INTERVAL/RNAseq/b37_b38_liftover/make_snp_map_%A_%a.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
-#SBATCH -a 1-22
 
 module load R
 Rscript /home/jm2294/projects/RNAseq/scripts/RNAseq/reformat_bgen/02_generate_snp_ids.R $SLURM_ARRAY_TASK_ID
