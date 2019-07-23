1f614064ff67a34a7350092a12f3548512640984
Jonathan Marten <jonmarten@gmail.com>
2019-07-23 11:20:08 +0100

    Increase memory limit

--- submit_limix_by_chunk_190618pipeline_noFilter.sh
+++ submit_limix_by_chunk_190618pipeline_noFilter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -A PETERS-SL3-CPU
-#SBATCH -p skylake
-#SBATCH --mem 40G
+#SBATCH -p skylake-himem
+#SBATCH --mem 50G
 #SBATCH --job-name=eqtl_chunktest_newpipe_nofilter
-#SBATCH --time=72:0:0
-#SBATCH --output=/rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/output_newpipeline_nofilter/eqtl_chunktest_newpipe_%A_%a.log
+#SBATCH --time=12:0:0
+#SBATCH --output=/rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/output_newpipeline_nofilter/500kb_window/eqtl_chunktest_newpipe_%A_%a.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
 
@@ -13,7 +13,7 @@
 start=$(date +%s.%N)
 
 # Get genomic positions for chunk
-CHUNK=$(head /rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/chunklist_b38.txt -n $SLURM_ARRAY_TASK_ID | tail -n 1)
+CHUNK=$(head /rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/chunklist_b38_50genes.txt -n $SLURM_ARRAY_TASK_ID | tail -n 1)
 #CHUNK=$(head /rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/chunklist.txt -n 529 | tail -n 1)  ## TEST LINE FOR INTERACTIVE RUN
 	
 CHR=$(echo $CHUNK | cut -d ' ' -f1)
@@ -24,13 +24,13 @@ END=$(echo $CHUNK | cut -d ' ' -f3)
 source activate limix_qtl
 
 # Specify file paths
-GENPATH=/rds/user/jm2294/hpc-work/projects/RNAseq/GENETIC_DATA/INTERVAL/RNAseq
+GENPATH=/home/jm2294/rds/hpc-work/projects/RNAseq/GENETIC_DATA
 PHEPATH=/rds/user/jm2294/hpc-work/projects/RNAseq
-OUTPATH=/rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/output_newpipeline_nofilter
+OUTPATH=/rds/user/jm2294/hpc-work/projects/RNAseq/test_run_chunks/output_newpipeline_nofilter/500kb_window
 
 # Specify files. NOTE THAT GENFILE DOES NOT NEED .bgen SUFFIX
 #GENFILE=${GENPATH}/impute_${CHR}_interval_RNAseq_batch1_withsamples_testfile_uniqueRSids
-GENFILE=${GENPATH}/b37_b38_liftover/impute_${CHR}_interval_b38_filtered_on_rsids
+GENFILE=${GENPATH}/b37_b38_liftover/impute_${CHR}_interval_b38_filtered
 ANFILE=${PHEPATH}/annotation_file/Feature_Annotation_Ensembl_gene_ids_autosomes_b38.txt
 PHEFILE=${PHEPATH}/test_run/phenotype_5281-fc-genecounts.txt
 SAMPLEMAPFILE=${PHEPATH}/test_run/sample_mapping_file_gt_to_phe.txt
@@ -39,14 +39,14 @@ GR=$(echo ${CHR}:${START}-${END})
 BLOCKSIZE=3000
 WINDOW=500000
 PERMUTATIONS=100
-MAF=0.002
+MAF=0.01
 
 # Echo config for log file
 echo Running Limix
 echo "************** Parameters **************"
 echo Chunk: $SLURM_ARRAY_TASK_ID
 echo Genomic Region: chr$GR
-echo Genotype File: $GENFILE
+echo Genotype File: ${GENFILE}.bgen
 echo Phenotype File: $PHEFILE
 echo Annotation File: $ANFILE
 echo Sample Map File: $SAMPLEMAPFILE
