dc35f0b38e70219f1520cd2c1588f2a8ef95fe57
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-07-24 16:13:13 +0100

    Convert bgen to hard-called plink

---
+++ reformat_bgen/reformat_to_plink.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -A PETERS-SL3-CPU
+#SBATCH -p skylake-himem
+#SBATCH --mem 20G
+#SBATCH --job-name=plink_convert_b38
+#SBATCH --time=12:0:0
+#SBATCH --output=/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/plink_bfile/logs/plink_convert_b38_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+
+
+#! This line enables the module command:
+. /etc/profile.d/modules.sh     
+
+module purge                  
+module load rhel7/default-peta4
+module load plink
+
+# get start time
+start=$(date +%s.%N)
+
+cd /rds/user/jm2294/hpc-work/projects/RNAseq/GENETIC_DATA/INTERVAL/RNAseq/b37_b38_liftover
+
+plink2\
+ --bgen /rds/user/jm2294/hpc-work/projects/RNAseq/GENETIC_DATA/b37_b38_liftover/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen\
+ --bfile /rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/plink_bfile/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered
+
+# get end time
+end=$(date +%s.%N)    
+runtime=$(python -c "print(${end} - ${start})")
+
+echo "Runtime was $runtime"
