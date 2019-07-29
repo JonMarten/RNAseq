08cb757744b609e66d6bee618293f37c1a182a44
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-07-29 14:35:08 +0100

    Modify to supply path as submission argument

--- postprocess_test.sh
+++ postprocess_test.sh
@@ -8,10 +8,17 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
 
+# NOTE: This script must be run with the path to the limix results as an argument
+if (( $# != 1 ))
+then
+  echo "Please supply the path to the results to be processed as an argment, eg [sbatch script.sh /path/to/folder]"
+  exit 1
+fi
+
 source activate limix_qtl
 
 # Set directory of results to merge
-RESULT_DIR=/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/00_testing/results/eqtl_test_13633258
+RESULT_DIR=$1
 OUTPUT_DIR=$(echo ${RESULT_DIR}/processed)
 mkdir -p $OUTPUT_DIR
 
