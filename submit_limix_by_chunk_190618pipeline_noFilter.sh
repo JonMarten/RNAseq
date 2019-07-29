f939e5d857e9545af700405b30bee987cf3c8110
Jonathan Marten <jonmarten@gmail.com>
2019-07-29 11:53:47 +0100

    Increase memory allowance

--- submit_limix_by_chunk_190618pipeline_noFilter.sh
+++ submit_limix_by_chunk_190618pipeline_noFilter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A PETERS-SL3-CPU
 #SBATCH -p skylake-himem
-#SBATCH --mem 100G
+#SBATCH --mem 120G
 #SBATCH --job-name=eqtl_test
 #SBATCH --time=12:0:0
 #SBATCH --output=/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/00_testing/logs/eqtl_test_%A_%a.log
