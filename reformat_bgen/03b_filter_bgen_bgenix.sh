e367596a3ce12a5ace9bc909ba2c789b1120dbed
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-07-30 11:40:08 +0100

    Add modules

--- reformat_bgen/03b_filter_bgen_bgenix.sh
+++ reformat_bgen/03b_filter_bgen_bgenix.sh
@@ -8,6 +8,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
 
+. /etc/profile.d/modules.sh     
+module purge
+module load rhel7/default-peta4
+module load bgen
+
 BGEN=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
 INCL=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt
 OUT=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/bgen_b38_filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen
