8c648229b40dc1ff18f474fdf3607e91b77bd970
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-07-30 12:06:11 +0100

    Split out indexing step

--- reformat_bgen/03a_index_bgen_bgenix.sh
+++ reformat_bgen/03a_index_bgen_bgenix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=filter_bgen_bgenix
+#SBATCH --job-name=index_bgen_bgenix
 #SBATCH -A PETERS-SL3-CPU
 #SBATCH -p skylake-himem
 #SBATCH --mem 20G
@@ -14,8 +14,6 @@ module load rhel7/default-peta4
 module load bgen
 
 BGEN=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
-INCL=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt
-OUT=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/bgen_b38_filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen
 
-# extract variants in lists
-bgenix -g $BGEN  -incl-rsids $INCL > $OUT
+# index bgen files
+bgenix -g $BGEN -index
