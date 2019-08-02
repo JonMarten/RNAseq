b074da2f777a3b3f8af41354d99e7be821145c50
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-02 10:17:38 +0100

    Increase memory allowance

--- reformat_bgen/02b_submit_snp_ids.sh
+++ reformat_bgen/02b_submit_snp_ids.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/logs/make_snp_map_%A_%a.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
-#SBATCH --mem 10G
+#SBATCH --mem 20G
 
 module load R
 Rscript /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/scripts/RNAseq/reformat_bgen/02_generate_snp_ids.R $SLURM_ARRAY_TASK_ID
