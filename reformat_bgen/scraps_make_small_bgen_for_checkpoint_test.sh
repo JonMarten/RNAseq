66537d9b221cb8fd595dc1bf5d51f7793c793ec4
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-05 16:14:23 +0100

    Test with small bgen

---
+++ reformat_bgen/scraps_make_small_bgen_for_checkpoint_test.sh
@@ -0,0 +1,7 @@
+qctool\
+ -g /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/impute_22_interval.bgen\
+ -s /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/interval.samples\
+ -incl-samples /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/analysis/00_testing/results/test_run/batch1_ids.txt\
+ -incl-range 22:23500000-24500000\
+ -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/test_bgen/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples.bgen
+	
