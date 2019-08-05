81835e73bd0bc21ee6be59fcd1ec7612f4473651
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-05 16:31:12 +0100

    Larger file, shorter interval

--- reformat_bgen/03c_update_bgen_checkpoint_test.sh
+++ reformat_bgen/03c_update_bgen_checkpoint_test.sh
@@ -18,8 +18,8 @@ ulimit -s 8192
 RESTARTSCRIPT="dmtcp_restart_script.sh"
 export DMTCP_QUIET=2
 
-runcmd='qctool -g /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/test_bgen/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples.bgen -map-id-data /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/INTERVAL_chr${SLURM_ARRAY_TASK_ID}_b37_to_b38_map.txt -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_checkpointtest_small.bgen'
-tint=300
+runcmd='qctool -g /home/jm2294/rds//home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/test_bgen/impute_22_20000000-30000000_interval_RNAseq_batch1_withsamples.bgen -map-id-data /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/INTERVAL_chr${SLURM_ARRAY_TASK_ID}_b37_to_b38_map.txt -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_checkpointtest_small.bgen'
+tint=60
 
 echo "Start coordinator"
 date
--- reformat_bgen/scraps_make_small_bgen_for_checkpoint_test.sh
+++ reformat_bgen/scraps_make_small_bgen_for_checkpoint_test.sh
@@ -2,6 +2,6 @@ qctool\
  -g /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/impute_22_interval.bgen\
  -s /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/interval.samples\
  -incl-samples /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/analysis/00_testing/results/test_run/batch1_ids.txt\
- -incl-range 22:23500000-24500000\
- -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/test_bgen/impute_22_23500000-24500000_interval_RNAseq_batch1_withsamples.bgen
+ -incl-range 22:20000000-30000000\
+ -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/test_bgen/impute_22_20000000-30000000_interval_RNAseq_batch1_withsamples.bgen
 	
