60c79cf134ea728222492ffddac075329eccc1c9
Jonathan Marten <jonmarten@gmail.com>
2019-08-07 17:00:32 +0100

    Update to SL2

--- reformat_bgen/03_qctool_update_bgen_files.sh
+++ reformat_bgen/03_qctool_update_bgen_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=update_bgen
-#SBATCH -A PETERS-SL3-CPU
+#SBATCH -A PAUL-SL2-CPU
 #SBATCH -p skylake-himem
-#SBATCH --mem 20G
-#SBATCH --time=12:0:0
+#SBATCH --mem 12G
+#SBATCH --time=24:0:0
 #SBATCH --output=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/logs/update_bgen_%A_%a.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
@@ -26,13 +26,6 @@ qctool\
  -map-id-data /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/INTERVAL_chr${SLURM_ARRAY_TASK_ID}_b37_to_b38_map.txt\
  -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
 
-# use unique identifiers to retain only non-duplicated SNPs mapped to b38
-qctool\
- -g /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen\
- -incl-snpids /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt\
- -s /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/interval.samples\
- -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/bgen_b38_filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen
- 
 end=$(date +%s.%N)
 
 runtime=$(python -c "print(${end} - ${start})")
