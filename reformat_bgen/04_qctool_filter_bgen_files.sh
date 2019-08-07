8aec137695b4136cd8d4d39e766b5d085522f1f4
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-07 11:25:57 +0100

    Script to filter bgen files for limix

---
+++ reformat_bgen/04_qctool_filter_bgen_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=update_bgen
+#SBATCH -A PETERS-SL3-CPU
+#SBATCH -p skylake-himem
+#SBATCH --mem 10G
+#SBATCH --time=12:0:0
+#SBATCH --output=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/logs/filter_bgen_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+
+# get start time
+start=$(date +%s.%N)
+
+. /etc/profile.d/modules.sh     
+module purge
+module load rhel7/default-peta4
+module load qctool
+
+# use unique identifiers to retain only non-duplicated SNPs mapped to b38
+qctool\
+ -g /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen\
+ -incl-snpids /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt\
+ -s /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/interval.samples\
+ -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/bgen_b38_filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen
+ 
+end=$(date +%s.%N)
+
+runtime=$(python -c "print(${end} - ${start})")
+
+echo "Runtime was $runtime"
