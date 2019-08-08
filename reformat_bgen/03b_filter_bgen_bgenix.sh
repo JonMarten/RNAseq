d8d5e46ad760dddce6cf56551835523ab9206058
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-08 12:01:55 +0100

    Update for running on new data

--- reformat_bgen/03b_filter_bgen_bgenix.sh
+++ reformat_bgen/03b_filter_bgen_bgenix.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=filter_bgen_bgenix
 #SBATCH -A PETERS-SL3-CPU
 #SBATCH -p skylake-himem
-#SBATCH --mem 20G
+#SBATCH --mem 12G
 #SBATCH --time=12:0:0
 #SBATCH --output=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/logs/update_bgen_bgenix_%A_%a.log
 #SBATCH --mail-type=ALL
@@ -13,9 +13,9 @@ module purge
 module load rhel7/default-peta4
 module load bgen
 
-BGEN=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
+BGEN=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
 INCL=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt
-OUT=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/bgen_b38_filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen
+OUT=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered.bgen
 
 # extract variants in lists
 bgenix -g $BGEN -incl-rsids $INCL > $OUT
