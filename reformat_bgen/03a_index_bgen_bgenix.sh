1c1cbbfab4ab919d9496c79fb741468b0ebffdd9
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-08 12:08:34 +0100

    Update paths

--- reformat_bgen/03a_index_bgen_bgenix.sh
+++ reformat_bgen/03a_index_bgen_bgenix.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=index_bgen_bgenix
 #SBATCH -A PETERS-SL3-CPU
 #SBATCH -p skylake-himem
-#SBATCH --mem 20G
+#SBATCH --mem 12G
 #SBATCH --time=12:0:0
 #SBATCH --output=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/logs/update_bgen_bgenix_%A_%a.log
 #SBATCH --mail-type=ALL
@@ -13,7 +13,7 @@ module purge
 module load rhel7/default-peta4
 module load bgen
 
-BGEN=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
+BGEN=/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38.bgen
 
 # index bgen files
 bgenix -g $BGEN -index
