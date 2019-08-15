f6a56b5e1d22a838b462f9a4b5dff9ed1476d36b
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-15 16:38:25 +0100

    Add leading zero to chr1-9 in annotation file

--- biomart_reformat_annotation_b38.R
+++ biomart_reformat_annotation_b38.R
@@ -1,5 +1,5 @@
 # Convert biomart output into annotation list for LIMIX use
-setwd("/home/jm2294/projects/RNAseq/annotation_file/")
+setwd("/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/analysis/00_testing/annotation_file")
 
 library(data.table)
 library(dplyr)
@@ -14,7 +14,14 @@ b <- a %>%
          feature_strand = Strand,
          gene_name = HGNC_symbol) %>%
   distinct() %>%
-  filter(chromosome %in% c(1:22, "X", "Y", "MT"))  %>% 
+  filter(chromosome %in% c(1:22))  %>% 
+  mutate(chromosome = as.numeric(chromosome)) %>%
   mutate(gene_name = ifelse(gene_name == "", feature_id, gene_name)) %>% # use ensembl gene id for genes with no HGNC symbol
-  filter(!duplicated(feature_id)) # remove 3 annotations where one gene has multiple transcripts. Retain one from each pair.
-fwrite(b, sep = "\t", "Feature_Annotation_Ensembl_gene_ids_autosomes_b38.txt")
+  filter(!duplicated(feature_id)) %>% # remove 3 annotations where one gene has multiple transcripts. Retain one from each pair. 
+  arrange(chromosome, start)
+
+b$chromosome <- as.character(b$chromosome)
+vec <- which(nchar(b$chromosome)==1)
+b$chromosome[vec] <- paste0("0", b$chromosome[vec])
+
+fwrite(b, sep = "\t", "Feature_Annotation_Ensembl_gene_ids_autosomes_b38.txt", quote = F)
