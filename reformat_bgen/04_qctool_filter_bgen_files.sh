3362b14eadb29206aa70ed9c44db9cb9a799a020
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-21 12:12:38 +0100

    Change file path to try and fix filtering

--- reformat_bgen/04_qctool_filter_bgen_files.sh
+++ reformat_bgen/04_qctool_filter_bgen_files.sh
@@ -19,7 +19,7 @@ module load qctool
 # use unique identifiers to retain only non-duplicated SNPs mapped to b38
 qctool\
  -g /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_no0.bgen\
- -incl-snpids /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt\
+ -incl-snpids /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/snp_inclusion_filters/c${SLURM_ARRAY_TASK_ID}_b38_filter_snps.txt\
  -s /home/jm2294/rds/rds-jmmh2-pre_qc_data/interval/affy_ukbiobank_array/raw_data/genetics/imputed/interval.samples\
  -og /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0.bgen
  
