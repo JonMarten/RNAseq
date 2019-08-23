b4a73cc514796ad78c954770cd9870a6a816a7ef
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-08-23 16:24:59 +0100

    Compare to eQTLgen

--- merge_processed_results.R
+++ merge_processed_results.R
@@ -9,16 +9,16 @@ library(ggplot2)
 files <- list.files()
 files <- files[grepl("processed_qtl_results", files)]
 
-for (file in files){
+for (file in files) {
   
   # if the merged dataset doesn't exist, create it
-  if (!exists("dat")){
+  if (!exists("dat")) {
     dat <- fread(file, data.table = F)
   }
   
   # if the merged dataset does exist, append to it
-  if (exists("dat")){
-    tempDat <-fread(file, data.table = F)
+  if (exists("dat")) {
+    tempDat <- fread(file, data.table = F)
     dat <- rbind(dat, tempDat)
     rm(tempDat)
   }
@@ -66,9 +66,14 @@ tops <- dat3 %>%
 
 bonfThresh <- 0.05 / nrow(tops)
 
-sigTops <- tops %>%
+sigTops.bonf <- tops %>%
   filter(empirical_feature_p_value < bonfThresh)
-eGenes <-sigTops %>% 
+eGenes.bonf <- sigTops.bonf %>% 
+  pull(feature_id)
+
+sigTops.BH <- tops %>%
+  filter(p_adjusted_BH < 0.05)
+eGenes.BH <- sigTops.BH %>%
   pull(feature_id)
 
 # Identify eSNPs within significant eGenes by selecting all SNPs with locally-adjusted P lower than the p-value threshold that corresponds to a global BH-adjusted P-value of 0.05
@@ -78,13 +83,13 @@ get_bh_threshold <- function(pvals, alpha, mtests = length(pvals)){
   m <- length(pvals)
   pvals <- sort(pvals)
   prejected <- which(pvals <= (1:m)/mtests*alpha)
-  ifelse(length(prejected)==0, 0, pvals[prejected[which.max(prejected)]])
+  ifelse(length(prejected) == 0, 0, pvals[prejected[which.max(prejected)]])
 }
 
 eSNPThresh <- get_bh_threshold(tops$empirical_feature_p_value, 0.05)
 
 eSNPs <- dat3 %>% 
-  filter(feature_id %in% eGenes & empirical_feature_p_value < eSNPThresh)
+  filter(feature_id %in% eGenes.BH & empirical_feature_p_value < eSNPThresh)
 
 fwrite(eSNPs, file = "significant_eSNPs_BH.csv")
 ###############
@@ -95,18 +100,49 @@ names(eQTL) <- paste0("eQTLgen_", names(eQTL))
 e22 <- eQTL %>% 
   filter(eQTLgen_SNPChr == 22) %>%
   mutate(matchID = paste0(eQTLgen_Gene, "_", eQTLgen_SNP))
-names(dat3) <- paste0("limix_", names(dat3))
-dat3 <- dat3 %>%
+dat4 <- dat3
+names(dat4) <- paste0("limix_", names(dat4))
+dat4 <- dat4 %>%
   mutate(matchID = paste0(limix_feature_id, "_", limix_rsid))
-merge <- inner_join(dat3, e22)
+merge <- inner_join(dat4, e22)
 
 mplot <- runif(3000, 1, nrow(merge))
 
 ggplot(merge[mplot,], aes(x = -log10(limix_p_value), y = -log10(eQTLgen_Pvalue))) + geom_point()
 
+cor(merge$limix_p_value, merge$eQTLgen_Pvalue,method = "spearman")
+
+# Check our hits in eqtlgen
+# eGenes
+eGene_replication <- e22 %>% 
+  group_by(eQTLgen_Gene) %>%
+  filter(eQTLgen_Gene %in% eGenes.BH) %>%
+  summarise(minP = min(eQTLgen_Pvalue)) %>% 
+  arrange(minP) %>%
+  data.frame() 
+
+# eSNPs
+eSNPs <- eSNPs %>%
+  mutate(matchID = paste0(feature_id, "_", rsid))
+eSNP_replication <- merge %>%
+  filter(matchID %in% eSNPs$matchID)
+
+# Count significant SNPs after bonf
+eSNP_replication %>%
+  filter(eQTLgen_Pvalue < 0.05/nrow(eSNP_replication)) %>%
+  nrow
+# Count significant SNPs after BH
+eSNP_replication %>%
+  mutate(p_adjusted_BH = p.adjust(eQTLgen_Pvalue, method = "BH")) %>%
+  filter(p_adjusted_BH < 0.05) 
+ 
+eSNP_replication <- eSNP_replication %>%
+  mutate(limix_zscore = limix_beta / limix_beta_se)
 
 
+ggplot(eSNP_replication, aes(x = abs(limix_zscore), y = abs(eQTLgen_Zscore))) + geom_point()
 
+cor(abs(eSNP_replication$limix_zscore), abs(eSNP_replication$eQTLgen_Zscore))
 
 
 
@@ -132,7 +168,7 @@ manhattan(filter(dat3, p_value < 0.0001), chr = "snp_chromosome", bp = "snp_posi
 
 
 # Merge with David's results to compare
-david <- fread("../../../../../v01_188id_preview/davids_calls/20PCs_Removed_eQTLsFDR0.05-SNPLevel.txt", data.table=F)
+david <- fread("../../../../../v01_188id_preview/davids_calls/20PCs_Removed_eQTLsFDR0.05-SNPLevel.txt", data.table = F)
 
 #remap <- fread("../../../../../GENETIC_DATA/b37_b38_liftover/from_savita_INTERVAL_imputed_liftover_hg38_cleaned/INTERVAL_imputed_liftover_hg38_cleaned_chr22.vcf", data.table=F)
 #names(remap) <- c("chr_b38","pos_b38","matchid", "ref_b38", "alt_b38")
@@ -144,7 +180,7 @@ d2 <- david %>%
   mutate(mergeid = paste0(ProbeName, "_", SNPName)) 
 
 beta <- str_split_fixed(d2$`Beta (SE)`, "\\(", 2)
-beta[,2] <- substr(beta[,2], 1, nchar(beta[,2])-1)
+beta[,2] <- substr(beta[,2], 1, nchar(beta[,2]) - 1)
 d2$beta <- as.numeric(beta[,1])
 d2$SE <- as.numeric(beta[,2])
 
@@ -162,7 +198,3 @@ ggplot(d3plot, aes(x = abs(beta.me), y = abs(beta.david))) +
 
 ggplot(d3plot, aes(x = -log10(p_value), y = -log10(PValue))) +
   geom_point()
-
-
-
-
