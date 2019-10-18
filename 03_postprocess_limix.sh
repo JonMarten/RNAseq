7d9d151bc3e5bf8700014ba06dbb4889c39a5dc5
Jonathan Marten <jonmarten@gmail.com>
2019-10-18 10:22:30 +0100

    Update postprocessing scripts for full run

---
+++ 03_postprocess_limix.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH -A PETERS-SL3-CPU
+#SBATCH -p skylake-himem
+#SBATCH --job-name=eqtl_merge_results
+#SBATCH --time=1:0:0
+#SBATCH --mem 5G
+#SBATCH --output=/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/00_testing/logs/eqtl_merge_results_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+
+# NOTE: This script must be run with the path to the limix results as an argument
+if (( $# != 1 ))
+then
+  echo "Please supply the path to the results to be processed as an argment, eg [sbatch script.sh /path/to/folder]"
+  exit 1
+fi
+
+source activate limix_qtl
+
+# Set directory of results to merge
+RESULT_DIR=$1
+OUTPUT_DIR=$(echo ${RESULT_DIR}/processed)
+mkdir -p $OUTPUT_DIR
+
+# Start runtime
+start=$(date +%s.%N)
+
+# Merge limix pipeline output files together into plain text format
+
+python /home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/hipsci_pipeline/post-processing_QTL/minimal_postprocess.py\
+ -id $RESULT_DIR\
+ -od ${OUTPUT_DIR}/processed_
+
+conda deactivate 
+
+# Print runtime
+end=$(date +%s.%N)    
+runtime=$(python -c "print(${end} - ${start})")
+echo "Runtime was $runtime"
--- reformat_bgen/reformat_to_plink_v02.sh
+++ reformat_bgen/reformat_to_plink_v02.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+#SBATCH -A PETERS-SL3-CPU
+#SBATCH -p skylake-himem
+#SBATCH --mem 20G
+#SBATCH --job-name=plink_convert_b38
+#SBATCH --time=12:0:0
+#SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/filtered/plink/logs/plink_convert_b38_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+
+. /etc/profile.d/modules.sh     
+module purge                  
+module load rhel7/default-peta4
 module load ceuadmin/plink/2.0_09_09_18
 
-cd /rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/bgen_b38_filtered
+cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/filtered
 
 plink2 \
- --bgen impute_22_interval_b38_filtered.bgen\
+ --bgen impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0_rnaSeqPhase1.bgen\
  --make-bed\
- --sort-vars\
- --out plink/impute_22_interval_b38_filtered
+ --out plink/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0_rnaSeqPhase1
