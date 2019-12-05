caaa6a04d2a61bc3d57f9e9db2ff01325ed5268e
Jonathan Marten <jm2294@medschl.cam.ac.uk>
2019-12-05 19:22:01 +0000

    Remove filter

--- 2_0_merge_plink_files.sh
+++ 2_0_merge_plink_files.sh
@@ -15,12 +15,9 @@ module load plink-1.9-gcc-5.4.0-sm3ojoi
 
 cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes
 
-# SNP filter list was generated from a failed merge run 3 SNPs with multiple positions and 35k multiallelic SNPS. Maybe retain these at a later date?
-
 plink\
  --merge-list /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/chr1_22_filelist.txt\
  --make-bed\
- --exclude /rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/multiallelic_filter_SNPs.txt\
- --out /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/INTERVAL_b38_autosomes_RNAseqPhase1_biallelic
+ --out /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/INTERVAL_b38_autosomes_RNAseqPhase1_biallelic_all
 
 
