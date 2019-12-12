924dae18c55fe5af9dbaade1b92f7bbdc6f79003
Jonathan Marten <jonmarten@gmail.com>
2019-12-12 11:58:06 +0000

    Test-run tensorQTL on a subset of features

---
+++ 2_2_tensorqtl_subset.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import tensorqtl
+from tensorqtl import genotypeio, cis, trans
+
+phenotype_bed_file = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/phenotypes/INTERVAL_RNAseq_phase1_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed.gz"
+covariates_file = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_PC10_PEER20.txt"
+plink_prefix_path = "/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/INTERVAL_b38_autosomes_RNAseqPhase1_biallelic_all"
+
+phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(phenotype_bed_file)
+covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T  # samples x covariates
+
+# subset phenotype db
+phe_df = phenotype_df.iloc[0:10]
+
+# Read in genotypes for subset only
+pr = genotypeio.PlinkReader(plink_prefix_path, select_samples=phe_df.columns)
+#pr = genotypeio.PlinkReader(plink_prefix_path)
+
+# load genotypes and variants into data frames
+genotype_df = pd.DataFrame(pr.get_all_genotypes(), index=pr.bim['snp'], columns=pr.fam['iid'])
+variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
+
+# cis mapping
+cis_df = cis.map_cis(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
+tensorqtl.calculate_qvalues(cis_df, qvalue_lambda=0.85)
