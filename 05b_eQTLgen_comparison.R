7fc83d454f573d1c389a7f5f5fafda29ae97cc24
Jonathan Marten <jonmarten@gmail.com>
2019-12-16 17:14:04 +0000

    Modify code for full cis results

--- 05b_eQTLgen_comparison.R
+++ 05b_eQTLgen_comparison.R
@@ -7,6 +7,8 @@ theme_set(theme_cowplot())
 
 setwd("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/01_cis_eqtl_mapping/results/cis_eqtls_18373genes_age_sex_rin_batch_PC10_PEER20_5GenesPerChunk/processed")
 
+prefix <- "cis_eqtls_18373genes_age_sex_rin_batch_PC10_PEER20"
+
 eSNPs <- fread("cis_eqtls_18373genes_age_sex_rin_batch_PC10_PEER20_eSNPs.txt", data.table = F)
 eQTLgen <- fread("zcat /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/eQTLgen/cis-eQTLs_full_20180905.txt.gz", data.table = F)
 
@@ -56,42 +58,55 @@ mismatchGenes <- eSNP_replication %>%
   filter(signMismatch == 1) %>%
   pull(feature_id) %>%
   unique()
-# Classify replication type
-eSNP_replication <- eSNP_replication %>%
-  mutate(Significant = ifelse(sig_bonf_sign == 1, "Bonf. & direction match", 
-                              ifelse(sigrep_bonf == 1 & sig_bonf_sign == 0, "Bonf. & direction mismatch",
-                                     "NS"))) %>%
-  mutate(Significant = factor(Significant, levels = c("NS","Bonf. & direction mismatch", "Bonf. & direction match")))
 
 # Classify SNPs as strand-ambiguous
 eSNP_replication$feature_length <- eSNP_replication$feature_end - eSNP_replication$feature_start
 eSNP_replication <- eSNP_replication %>%
-  mutate(ambig = ifelse(eQTLgen_AssessedAllele == "A" & eQTLgen_OtherAllele == "T" |
-                          eQTLgen_AssessedAllele == "T" & eQTLgen_OtherAllele == "A" |
-                          eQTLgen_AssessedAllele == "C" & eQTLgen_OtherAllele == "G" |
-                          eQTLgen_AssessedAllele == "G" & eQTLgen_OtherAllele == "C", 1, 0)) %>%
+  mutate(ambig = ifelse(assessed_allele == "A" & ref_allele == "T" |
+                          assessed_allele == "T" & ref_allele == "A" |
+                          assessed_allele == "C" & ref_allele == "G" |
+                          assessed_allele == "G" & ref_allele == "C", 1, 0)) %>%
   mutate(veryambig = ifelse(ambig == 1 & maf > 0.45, 1, 0))
 
-# get list of features with more than one significant SNPs mismatched  
-flexmismatchGenes <- eSNP_replication %>%
-  filter(Significant  == "Bonf. & direction mismatch") %>%
+# Sort eSNP list 
+eSNP_replication <- eSNP_replication %>% 
+  arrange(feature_chromosome, feature_start, snp_position)
+
+# get list of features with more than 2 significant SNPs mismatched  
+mismatches <- eSNP_replication %>%
+  filter(P.BH.eqtlgen < 0.05 & signMismatch  == 1) %>%
   group_by(feature_id) %>%
-  summarise(n_mismatched = n()) %>%
-  data.frame %>%
-  filter(n_mismatched > 1) %>%
+  summarise(n_mismatched = n(), chr = unique(feature_chromosome)) %>%
+  data.frame 
+
+flexmismatchGenes <- mismatches %>%
+  filter(n_mismatched > 2) %>%
   pull(feature_id)
 
 #Plot Z scores
-g <- ggplot(eSNP_replication, aes(x = zscore, y = eQTLgen_Zscore_flipped, color = Significant)) + 
-  geom_point(cex = 0.8, alpha = 0.5, pch = 20) +
+
+g <- ggplot(eSNP_replication, aes(x = zscore, y = Zscore.eqtlgen.flipped)) + 
+  geom_bin2d(bins = 100) +
   theme(aspect.ratio = 1, legend.position = "right") +
-  scale_colour_manual(values = c("gray75", "orangered", "chartreuse3")) +
   geom_vline(xintercept = 0) + 
   geom_hline(yintercept = 0) + 
+  scale_fill_continuous(type = "viridis") +
   labs(x = "Limix Z-score", y = "eQTLgen Z-score") 
 
-outname_plot <- paste0(jobdf$job_id[i],"/processed/",jobdf$job_id[i], "_eSNPs_eqtlgenReplication_zscoreplot.png")
+outname_plot <- paste0(prefix, "_eSNPs_eqtlgenReplication_zscoreplot.png")
 save_plot(file = outname_plot, g, base_height = 10, base_width = 10)
+
+# plot single gene
+onegene <- ggplot(filter(eSNP_replication, feature_id == "ENSG00000229391"), 
+             aes(x = zscore, y = eQTLgen_Zscore_flipped, color = Significant)) + 
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  geom_point(cex = 0.8, alpha = 1, pch = 20) +
+  theme(aspect.ratio = 1, legend.position = "right") +
+  labs(x = "Limix Z-score", y = "eQTLgen Z-score") +
+  theme_minimal() +
+  scale_colour_manual(values = c("gray75", "orangered", "chartreuse3")) +
+  facet_wrap(. ~ gene_name)  
 
 
 # plot mismatches coloured by significance 
