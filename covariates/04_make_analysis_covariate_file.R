459463b84b288b14aee167fd7e4f816edbd78a58
Jonathan Marten <jonmarten@gmail.com>
2020-02-04 13:36:18 +0000

    Output GxE interaction term file

--- covariates/04_make_analysis_covariate_file.R
+++ covariates/04_make_analysis_covariate_file.R
@@ -32,5 +32,8 @@ fwrite(covout.peer, file = "INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_P
 fwrite(covout.bloodcells, file = "INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10_NeutPCT_LympPCT_MonoPCT_EoPCT_BasoPCT.txt", sep = "\t")
 
 # Make GxE interaction covariate file
+gxeOut <- allcovs %>%
+  select(sample_id, NEUT_PCT___RNA)
+fwrite(gxeOut, file = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", sep = "\t")
 
 
