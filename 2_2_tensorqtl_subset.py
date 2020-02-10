4fea4d2dc22ba6bd445ccfa4a6c15052202f95ba
Jonathan Marten <jonmarten@gmail.com>
2020-02-10 11:42:47 +0000

    Run TensorQTL from a python script

--- 2_2_tensorqtl_subset.py
+++ 2_2_tensorqtl_subset.py
@@ -4,26 +4,33 @@ from tensorqtl import genotypeio, cis, trans
 
 phenotype_bed_file = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/phenotypes/INTERVAL_RNAseq_phase1_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed.gz"
 covariates_file = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_PC10_PEER20.txt"
-#plink_prefix_path = "/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/INTERVAL_b38_autosomes_RNAseqPhase1_biallelic_all"
-plink_prefix_path = "/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/INTERVAL_b38_autosomes_RNAseqPhase1_biallelic_all_MAF0.05"
+plink_prefix_path = "/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/genotypes/INTERVAL_b38_autosomes_RNAseqPhase1_biallelic_all_MAF0.005"
+outdir = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/results/python_module_method/"
 
 phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(phenotype_bed_file)
 covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T  # samples x covariates
 
-interaction_df = pd.read_csv("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", sep = "\t", index_col=0, squeeze = True).T
+interaction_s = pd.read_csv("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", sep = "\t", index_col=0, squeeze = True).T
 
-# subset phenotype db
-#phe_df = phenotype_df.iloc[0:10]
-
-# Read in genotypes for subset only
-#pr = genotypeio.PlinkReader(plink_prefix_path, select_samples=phe_df.columns)
+# Read in genotypes 
 pr = genotypeio.PlinkReader(plink_prefix_path)
 
 # load genotypes and variants into data frames
 genotype_df = pd.DataFrame(pr.get_all_genotypes(), index=pr.bim['snp'], columns=pr.fam['iid'])
 variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
 
-# cis mapping
-cisnom_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="Test_gxe", interaction_s=interaction_df)
+# Cis gene-level mapping
 cis_df = cis.map_cis(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
 tensorqtl.calculate_qvalues(cis_df, qvalue_lambda=0.85)
+cis_df.to_csv(outdir + "tensorqtl_cis_MAF0.005_cisPerGene.csv", index=False)
+
+# Cis nominal mapping
+cisnom_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix=outdir + "tensorqtl_cis_MAF0.005_cisNominal")
+
+# Conditional analysis
+indep_df = cis.map_independent(genotype_df, variant_df, cis_df, phenotype_df, phenotype_pos_df, covariates_df)
+indep_df.to_csv(outdir + "tensorqtl_cis_MAF0.005_cisIndependent.csv", index=False)
+
+# GxE
+cisGxE_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="Test_gxe", interaction_s=interaction_df)
+cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="tensorqtl_cis_MAF0.005_cisGxE",interaction_s=interaction_s, maf_threshold_interaction=0.05,group_s=None, run_eigenmt=True, output_dir=outdir)
---
+++ 2_2_tensorqtl_subset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/logs/TensorQTL_pythonmodule_test_%A.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+#SBATCH -J TensorQTL_pythonmodule_test
+#SBATCH -A PAUL-SL3-GPU
+#SBATCH --time=12:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks=4
+#SBATCH --gres=gpu:4
+#SBATCH -p pascal
+
+#! ############################################################
+. /etc/profile.d/modules.sh
+module purge
+module load rhel7/default-gpu
+module load miniconda3-4.5.4-gcc-5.4.0-hivczbz 
+module load cuda/9.2
+module load r-3.6.0-gcc-5.4.0-bzuuksv
+
+source activate tensorQTL
+
+python /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/scripts/RNAseq/2_2_tensorqtl_subset.py
