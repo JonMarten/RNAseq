a9ddcdaad1d12ad0151dbddd9621b4fbbfb55f5d
Jonathan Marten <jonmarten@gmail.com>
2020-04-07 17:19:37 +0100

    Update for phase 2

--- genotypes/1_5_reformat_to_plink.sh
+++ genotypes/1_5_reformat_to_plink.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem 20G
 #SBATCH --job-name=plink_convert_b38
 #SBATCH --time=12:0:0
-#SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/filtered/plink/logs/plink_convert_b38_%A_%a.log
+#SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/logs/genotype_processing/plink_convert_b38_%A_%a.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jm2294@medschl.cam.ac.uk
 
@@ -13,9 +13,9 @@ module purge
 module load rhel7/default-peta4
 module load ceuadmin/plink/2.0_09_09_18
 
-cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/GENETIC_DATA/b37_b38_liftover/b38_bgen/filtered
+cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/genotypes
 
 plink2 \
- --bgen impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0_rnaSeqPhase1.bgen\
+ --bgen processing/b38_bgen/filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0_rnaSeqPhase1.bgen\
  --make-bed\
- --out plink/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0_rnaSeqPhase1
+ --out INTERVAL_chr${SLURM_ARRAY_TASK_ID}_imputed_b38_filtered_rnaSeq
