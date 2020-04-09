ae6fa9c54befb936e5c937180e644e88d013b461
Jonathan Marten <jonmarten@gmail.com>
2020-04-09 15:08:06 +0100

    Add filters for HWE, MAF and multiallelic SNPs

--- genotypes/1_5_reformat_to_plink.sh
+++ genotypes/1_5_reformat_to_plink.sh
@@ -11,11 +11,14 @@
 . /etc/profile.d/modules.sh     
 module purge                  
 module load rhel7/default-peta4
-module load ceuadmin/plink/2.0_09_09_18
+module load plink-1.9-gcc-5.4.0-sm3ojoi
 
 cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/genotypes
 
-plink2 \
+plink\
  --bgen processing/b38_bgen/filtered/impute_${SLURM_ARRAY_TASK_ID}_interval_b38_filtered_no0_rnaSeqPhase1_2.bgen\
  --make-bed\
- --out INTERVAL_chr${SLURM_ARRAY_TASK_ID}_imputed_b38_filtered_rnaSeq
+ --maf 0.005\
+ --hwe 1e-6\
+ --exclude processing/snp_inclusion_filters/plink_multiallelic_filter_SNPs.txt\
+ --out INTERVAL_RNAseq_Phase1-2_imputed_b38_biallelic_MAF0.005_chr${SLURM_ARRAY_TASK_ID}
