f808fef73fd738807197014816cab74e8484c5d0
Jonathan Marten <jonmarten@gmail.com>
2020-04-14 17:19:03 +0100

    Update phentoype scripts for phase 2

---
+++ 3_1_make_tensorQTL_input_phase2.R
@@ -0,0 +1,108 @@
+# make bed format phenotypes for tensorQTL
+
+#Chr start end ID UNR1 UNR2 UNR3 UNR4 
+#chr1 173863 173864 ENSG123 -0.50 0.82 -0.71 0.83
+#chr1 685395 685396 ENSG456 -1.13 1.18 -0.03 0.11
+#chr1 700304 700305 ENSG789 -1.18 1.32 -0.36 1.26
+
+library(data.table)
+library(dplyr)
+
+setwd("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/phenotypes")
+
+# Create mapping file to match phenotype to genotype
+omictable <- fread("../covariates/processed/INTERVAL_omics_table_02APR2020.csv", data.table = F)
+idmap <- omictable %>%
+  select(genotype_individual_id = affymetrix_ID, phenotype_individual_id = RNA_ID) %>%
+  filter(!is.na(phenotype_individual_id))
+
+# Read in TMM normalised FPKM feature counts
+phe <- fread("processed/filteredSamplesGenes_TMMNormalised_FPKM_Counts_Phase1-2_initialcalling.csv", data.table = F)
+
+anno <- fread("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/01_cis_eqtl_mapping/annotation_file/Feature_Annotation_Ensembl_gene_ids_autosomes_b38.txt", data.table = F)
+
+bed <- left_join(phe, anno[,1:4]) %>%
+  select(Chr = chromosome, start, end, ID = feature_id, INT_RNA7427205:INT_RNA7960548) %>%
+  arrange(Chr, start) %>%
+  filter(!is.na(Chr)) %>%
+  rename("#Chr" = Chr)
+
+# Identify missing IDs
+missingIDs <- names(bed)[which(!names(bed) %in% idmap$phenotype_individual_id)][-c(1:4)]
+cov <- fread("../covariates/processed/INTERVAL_RNA_batch1-12_master_covariates_release_2020_04_02.csv", data.table = F)
+write.table(missingIDs, file = "RNAseq_phase1-2_missing_phenotype_IDs.txt", quote = F, col.names = F, row.names = F)
+
+# Rename IDs to match genotype file
+namevec <- base::match(names(bed)[5:ncol(bed)], idmap$phenotype_individual_id)
+names(bed)[5:ncol(bed)] <-  as.character(idmap$genotype_individual_id[namevec])
+
+# sort pheno file IDs
+sortedids <- names(bed)[-c(1:4)] %>% sort
+bed2 <- bed %>%
+  select("#Chr", start, end, ID, sortedids)
+
+fwrite(bed, sep = "\t", file = "INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_initialcalling_foranalysis.bed")
+
+for(i in 1:22) {
+  bedChr <- bed %>%
+    rename(Chr = "#Chr") %>%
+    filter(Chr == i)%>%
+    rename("#Chr" = Chr)
+  
+  fwrite(bedChr, sep = "\t", file = paste0("INTERVAL_RNAseq_phase1_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr",i,".bed"))
+  rm(bedChr)
+}
+
+###########################################
+## NOTE: The bed file must be compressed and indexed with the commands below:
+# module load ceuadmin/tabix/0.2.6
+# bgzip INTERVAL_RNAseq_phase1_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed && tabix -p bed INTERVAL_RNAseq_phase1_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed.gz
+############################################
+
+# Make Covariate file
+#id UNR1 UNR2 UNR3 UNR4
+#PC1 -0.02 0.14 0.16 -0.02
+#PC2 0.01 0.11 0.10 0.01
+#PC3 0.03 0.05 0.08 0.07
+#BIN 1 0 0 1
+
+# Function to reformat covariate files for TensorQTL
+makeCov <- function(file, idmap, bed) {
+  cov <- fread(file, data.table = F)
+  cov <- cov %>%
+    mutate(sample_id =  as.character(idmap$genotype_individual_id[base::match(sample_id, idmap$phenotype_individual_id)]))
+  tcov <- transpose(cov)
+  names(tcov) <- tcov[1,] 
+  tcov <- tcov[-1,]
+  tcov$id <- names(cov)[-1]
+  # remove IDs missing in phenotype file
+  remIDs <- names(tcov)[which(!names(tcov) %in% names(bed))][-1]
+  tcov <- select(tcov, -remIDs)
+  tcov <- select(tcov, id, sortedids) 
+}
+
+
+covMinimal <- makeCov("../../01_cis_eqtl_mapping/covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10.txt", idmap, bed)
+covPEER <- makeCov("../../01_cis_eqtl_mapping/covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10_PEER20.txt", idmap, bed)
+covCellPct <- makeCov("../../01_cis_eqtl_mapping/covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10_NeutPCT_LympPCT_MonoPCT_EoPCT_BasoPCT.txt", idmap, bed)
+
+fwrite(covMinimal, file = "../covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10.txt", sep = "\t")
+fwrite(covPEER, file = "../covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10_PEER20.txt", sep = "\t")
+fwrite(covCellPct, file = "../covariates/INTERVAL_RNAseq_phase1_age_sex_rin_batch_readDepth_PC10_NeutPCT_LympPCT_MonoPCT_EoPCT_BasoPCT.txt", sep = "\t")
+
+# GxE file
+# currently replcaed NAs with the median, just to see if these are screwing up the GxE
+intr <- fread("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/01_cis_eqtl_mapping/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", data.table = F)
+intr <- intr %>%
+  mutate(sample_id =  as.character(idmap$genotype_individual_id[base::match(sample_id, idmap$phenotype_individual_id)]),
+         NEUT_PCT___RNA = ifelse(is.na(NEUT_PCT___RNA), median(NEUT_PCT___RNA, na.rm=T), NEUT_PCT___RNA))
+tintr <- transpose(intr)
+names(tintr) <- tintr[1,] 
+tintr <- tintr[-1,]
+tintr$id <- names(intr)[-1]
+tintr <- select(tintr, id, sortedids) 
+# remove IDs missing in phenotype file
+remIDs <- names(tintr)[which(!names(tintr) %in% names(bed))][-1]
+tintr <- select(tintr, -remIDs)
+fwrite(tintr, file = "/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", sep = "\t")
+
--- artika_TMM_normalisation.R
+++ artika_TMM_normalisation.R
@@ -16,27 +16,17 @@ library(dplyr)
 # ----------------------
 # set working directory
 # ----------------------
-setwd("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/covid19")
+setwd("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/phenotypes/")
 
 # -----------------------------------------------------------------------------
 # Read in the data from all the batches, annotation file, metadata file
 # -----------------------------------------------------------------------------
 
 # read the meta-data
-metadata <- read.csv("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/01_cis_eqtl_mapping/covariates/INTERVAL_RNA_batch1-8_covariates_release_2019_08_15.csv")
+metadata <- read.csv("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/covariates/processed/INTERVAL_RNA_batch1-12_master_covariates_release_2020_04_02.csv")
 
-path <- "/rds/project/jmmh2/rds-jmmh2-pre_qc_data/interval/rna_seq/raw_data/globus_phase1/"
-
-# read the count data from each batch and store in a list 
-batch1 <- fread(paste0(path,  "batch1/matrices/counts.FC.unstranded.5281-tic109.txt"),  data.table = F)
-batch2 <- fread(paste0(path, "batch2/results-study5591-tic109b/combined/study5591-tic109b-star-fc-genecounts.txt"), data.table = F)
-colnames(batch2) <- gsub(".star.gene.fc.txt", "", colnames(batch2))
-batch3 <- fread(paste0(path, "batch3/results-study5591-tic109d/combined/study5591-tic109d-star-fc-genecounts.txt"), data.table = F)
-batch4 <- fread(paste0(path, "batch4/results-study5591-tic276/combined/study5591-tic276-star-fc-genecounts.txt"), data.table = F)
-batch5 <- fread(paste0(path, "batch5/results-study5591-tic328/combined/study5591-tic328-star-fc-genecounts.txt"), data.table = F)
-batch6 <- fread(paste0(path, "batch6/results-study5591-tic297/combined/study5591-tic297-star-fc-genecounts.txt"), data.table = F)
-batch7 <- fread(paste0(path, "batch7/results-study5591-tic329/combined/study5591-tic329-star-fc-genecounts.txt"), data.table = F)
-batch8 <- fread(paste0(path, "batch8/results-study5591-tic364/combined/study5591-tic364-star-fc-genecounts.txt"), data.table = F)
+# Read in the feature counts
+dat <- fread("raw/interval_basic-star-fc-genecounts.txt", data.table = F)
 
 # read the annotation file
 #annotation <- read.csv("~/Dropbox/Artika/Projects/INTERVAL/Analysis/Batches1_8/GeneAnnotationFile_GTF/GeneAnnotationFile_EnsembltoGeneIds.csv")
@@ -51,14 +41,6 @@ ACE2 <- data.frame("feature_id" = "ENSG00000130234",
                    "gene_name" = "ACE2",
                    "gene_length" = 41115)
 annotation <- rbind(annotation, ACE2)
-
-#---------------------------------------------------------------- 
-# Combine the batches together 
-#----------------------------------------------------------------
-exp <- list(batch1, batch2[, -1], batch3[, -1], batch4[,-1], batch5[,-1], batch6[, -1], batch7[, -1], batch8[,-1])
-#names(exp) <- c("batch1", "batch2", "batch3", "batch4", "batch5", "batch6", "batch7", "batch8")
-dat <- do.call(cbind, exp)
-fwrite(dat, "counts_batches_1_8_raw.csv", sep = ",")
 
 # Get the gene symbols corresponding to ENSEMBLE IDs and gene length   
 counts <- merge(annotation, dat, by.x = "feature_id", by.y = "ENSEMBL_ID")
@@ -79,10 +61,10 @@ remove_RIN_Less4 <- as.character(metadata$sample_id[which(metadata$RIN < 4)])
 #Remove samples with ReadDepth Less than 10
 # There are N=5 individuals
 ReadDepth <- colSums(counts[,12:ncol(counts)])
-#remove_ReadDepth_lessthan_10Million <- names(ReadDepth)[which(ReadDepth < 10000000)]
+remove_ReadDepth_lessthan_10Million <- names(ReadDepth)[which(ReadDepth < 10000000)]
 
 # Total remove N=28 individuals
-remove_ind <- c(remove_batch2, remove_RIN_Less4)
+remove_ind <- c(remove_batch2, remove_RIN_Less4, remove_ReadDepth_lessthan_10Million)
 
 # -----------------------------------------------------------------------------
 # Create DGEList object y and remove outlier genes, using the FC data
@@ -95,11 +77,10 @@ y <- edgeR::DGEList(counts=counts1[,8:ncol(counts1)], genes=counts1[,1:7])
 # -------------
 # Select genes with > 0.5 CPM in at least 10% of the samples
 keep <- rowSums(cpm(y) > 0.5) >= round(ncol(y$counts)*0.1) #10% is N=276 individuals
-#keep3 <- rowSums(cpm(y) > 1) >= round(ncol(y$counts)*0.1)
 
 summary(keep)
-#y1 <- y[keep, , keep.lib.sizes = FALSE]
-y1 <- y
+y1 <- y[keep, , keep.lib.sizes = FALSE]
+#y1 <- y
 # TMM Normalisation
 y2 <- calcNormFactors(y1)
 
@@ -109,7 +90,7 @@ rpkms <- rpkm(y2, normalized.lib.sizes=TRUE, log=TRUE, prior.count=0.25, gene.le
 
 genes <- y2$genes[, c("feature_id", "gene_name", "chromosome", "start", "end","gene_length")]
 rpkms1 <- cbind(genes, rpkms)
-fwrite(rpkms1, "UNfilteredSamplesGenes_TMMNormalised_FPKM_Counts.csv", sep = ",")
+fwrite(rpkms1, "processed/filteredSamplesGenes_TMMNormalised_FPKM_Counts_Phase1-2_initialcalling.csv", sep = ",")
 
 
 
