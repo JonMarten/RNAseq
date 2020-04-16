1ecad9614835320ad6eb2ec87e795f9c117ce26f
Jonathan Marten <jonmarten@gmail.com>
2020-04-16 17:04:55 +0100

    Update with new technical covariates

--- covariates/1_1_merge_technical_covariates.R
+++ covariates/1_1_merge_technical_covariates.R
@@ -4,16 +4,11 @@ library(data.table)
 library(dplyr)
 
 techCovsPhase1 <- fread("raw/INTERVAL_RNA_Covariates_09-08-2019.csv", data.table = F)
-techCovsPhase2 <- fread("raw/INTERVAL_RNA_Covariates_Ph2_27-03-2020.csv", data.table = F)
+techCovsPhase2 <- fread("raw/INTERVAL_RNA_Covariates_Ph2_15-04-2020.csv", data.table = F)
 
 techCovsPhase2 <- techCovsPhase2 %>% 
   mutate_at(c("Sample_ID", "Lane", "Freezer_Shelf", "Agilent_RINe"), as.character)
-  
-  
-  
-  mutate(Sample_ID = as.character(Sample_ID),
-         Lane = as.character(Lane))
 
 techCovsAll <- bind_rows(techCovsPhase1, techCovsPhase2)
 
-fwrite(techCovsAll, "processed/INTERVAL_RNA_technical_covariates_batch1-12_20200402.csv")
+fwrite(techCovsAll, "processed/INTERVAL_RNA_technical_covariates_batch1-12_20200416.csv")
