6bcddba09fbc4022148b2610b28f59cc70b4ec41
Jonathan Marten <jonmarten@gmail.com>
2020-04-24 17:29:21 +0100

    Add sbatch parameters

--- covid-19/1_2_filter_chrx.sh
+++ covid-19/1_2_filter_chrx.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#SBATCH -A PAUL-SL3-CPU
+#SBATCH -p skylake-himem
+#SBATCH --mem 50G
+#SBATCH --job-name=filter_chrx
+#SBATCH --time=12:0:0
+#SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/covid19/genotypes/plink_filter_log_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
 
 . /etc/profile.d/modules.sh     
 module purge                  
@@ -7,11 +15,8 @@ module load plink-1.9-gcc-5.4.0-sm3ojoi
 
 cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/covid19/genotypes
 
-cp /home/jm2294/rds/rds-jmmh2-projects/covid/ace2/interval_genetic_data/interval_imputed_data/INT_X_filt_merged.vcf.gz .
-gunzip INT_X_filt_merged.vcf.gz
-
 plink\
- --vcf INT_X_filt_merged.vcf\
+ --vcf /home/jm2294/rds/rds-jmmh2-projects/covid/ace2/interval_genetic_data/interval_imputed_data/INT_X_filt_merged.vcf.gz\
  --chr 23\
  --keep /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/genotypes/processing/rna_seq_phase1-2_affy_ids.txt\
  --make-bed\
