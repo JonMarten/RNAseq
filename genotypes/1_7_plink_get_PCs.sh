d11bc165bb5e9e5763b5a4cea430e9692b6bab37
Jonathan Marten <jonmarten@gmail.com>
2020-05-19 15:45:45 +0100

    Change CSD3 account

--- genotypes/1_7_plink_get_PCs.sh
+++ genotypes/1_7_plink_get_PCs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=plink_calc_PCs_benscript
 #SBATCH -p skylake-himem
-#SBATCH -A PETERS-SL3-CPU
+#SBATCH -A PAUL-SL3-CPU
 #SBATCH --time=12:0:0
 #SBATCH --mem=15G
 #SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/logs/genotype_processing/plink_calc_PCs_%A.log
