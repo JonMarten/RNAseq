d27fd20d538d70ebb4cdcbdaed6f5063cf8bcbfd
Jonathan Marten <jonmarten@gmail.com>
2020-07-20 15:20:04 +0100

    Update bed files to use latest phenotype data

--- 3_2_index_bed.sh
+++ 3_2_index_bed.sh
@@ -6,10 +6,10 @@
 module load ceuadmin/tabix/0.2.6
 cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/phenotypes
 
-bgzip INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_initialcalling_foranalysis.bed && tabix -p bed INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_initialcalling_foranalysis.bed.gz
+bgzip INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed && tabix -p bed INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed.gz
 
 # Compress and index per-chromosome files for conditional analysis
 for i in {1..22}
 do
-  bgzip INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_initialcalling_foranalysis_chr${i}.bed && tabix -p bed INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_initialcalling_foranalysis_chr${i}.bed.gz
+  bgzip INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr${i}.bed && tabix -p bed INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr${i}.bed.gz
 done
