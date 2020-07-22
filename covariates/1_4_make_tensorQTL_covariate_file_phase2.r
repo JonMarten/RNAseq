d7938d10c0ba98e95857d8ae8a305bc7aed62162
Jonathan Marten <jonmarten@gmail.com>
2020-07-22 16:47:31 +0100

    Add peer factors for covariate file

--- covariates/1_4_make_tensorQTL_covariate_file_phase2.r
+++ covariates/1_4_make_tensorQTL_covariate_file_phase2.r
@@ -6,38 +6,80 @@
 #BIN 1 0 0 1
 library(data.table)
 library(dplyr)
+library(fastDummies)
 setwd("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/covariates")
 
-cov <- fread(data.table = F, "processed/INTERVAL_RNA_batch1-12_master_covariates_release_2020_04_02.csv")
-phe <- fread(data.table = F, "zcat ../phenotypes/INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_initialcalling_foranalysis_chr22.bed.gz")
+cov <- fread(data.table = F, "processed/INTERVAL_RNA_batch1-12_master_covariates_release_2020_07_01.csv")
+phe <- fread(data.table = F, "zcat ../phenotypes/INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr22.bed.gz")
 
 PCs <- fread("../genotypes/PCs/rnaseq_phase1.typed.maf1pc.geno3pc.hwe1minus5.complex-region-excl.indep-50-5-1.5.pruning.genome.pca.eigenvec", data.table = F) %>%
   select(-"#FID") %>%
   rename(affymetrix_ID = IID)
 cov <- left_join(cov, PCs)
 
-# NEED TO ADD IN READ DEPTH
+# Needs updating to use latest files in peer_30Fact_100Iter_sampleSwapsFixed when this has finished running, this is for prototyping purposes 
+peer <- fread("../peer_factors/peer_30Fact_100Iter_sampleSwapsFixed/PEER_factors.txt", data.table = F)
+peer <- peer %>%
+  rename(sample_id = V1) %>%
+  select(sample_id, PEER1:PEER20)
+
+cov <- left_join(cov, peer)
+
+# make dummy cols
+batchCols <- cov %>% 
+  select(sequencingBatch) %>%
+  mutate(sequencingBatch = as.factor(sequencingBatch)) %>%
+  dummy_cols
+
+# Create a variable for season that is numerically sequential and equally spaced - an interger corresponding to sample colleciton month and year. This is to stop 201512 having a huge value jump to 201601
+datelevels <- paste0(c(rep(2015, 12), rep(2016, 12)), sprintf("%02d", 1:12))
+cov <- cov %>%
+  mutate(RNA_sample_year = substr(attendanceDate___RNA, 1,4)) %>%
+  mutate(yearmonth = paste0(RNA_sample_year, sprintf("%02d", RNA_sample_month))) %>%
+  mutate(season = as.numeric(factor(yearmonth, levels = datelevels)))
+
+cov <- cbind(cov, batchCols[,-1])
+
+# Select analysis covariates
 cov2 <- cov %>%
   mutate(affymetrix_ID = as.character(affymetrix_ID)) %>%
   filter(affymetrix_ID %in% names(phe)) %>%
-  select(id = affymetrix_ID, age_RNA, sex, Agilent_RINe, sequencingBatch, PC1:PC10) %>%
+  select(id = affymetrix_ID, 
+         age_RNA, 
+         sex,
+         BMI, 
+         season,
+         Agilent_RINe,
+         sequencingBatch_1:sequencingBatch_12,
+         Conc_ng_ul,
+         BASO_PCT___RNA, 
+         EO_PCT___RNA, 
+         HCT_PCT___RNA, 
+         HGB_g_dL___RNA, 
+         IRF_PCT___RNA, 
+         LYMPH_PCT___RNA, 
+         MCH_pg___RNA, 
+         MCHC_g_dL___RNA, 
+         MCV_fL___RNA, 
+         MONO_PCT___RNA, 
+         MPV_fL___RNA, 
+         NEUT_PCT___RNA, 
+         PCT_PCT___RNA, 
+         PDW_fL___RNA, 
+         PLT_F_10_9_L___RNA, 
+         RBC_10_12_L___RNA, 
+         RDW_SD_fL___RNA, 
+         RET_PCT___RNA, 
+         WBC_10_9_L___RNA,
+         PC1:PC10,
+         PEER1:PEER20
+         ) %>%
   mutate(sex = as.numeric(gsub(2, 0, sex)),
          RIN = as.numeric(ifelse(Agilent_RINe == "", NA, Agilent_RINe))) %>%
   filter(!is.na(RIN))
 
-# convert batch to dummy variables
-library(varhandle)
-batch <- to.dummy(cov2$sequencingBatch, prefix = "batch") %>%
-  data.frame()
-names(batch) <- gsub("\\.", "", names(batch))
-cov2 <- cbind(cov2, batch)
-
-covOut <- cov2 %>%
-  select(id:sex, RIN, paste0("batch", 1:12), PC1:PC10)
-
-covOut.t <- covOut %>%
+covOut.t <- cov2 %>%
   t %>%
   data.frame(stringsAsFactors = F)
 
-
-write.table(covOut.t, sep = "\t", quote = F, col.names = F, row.names = T, file = "INTERVAL_RNAseq_phase1-2_age_sex_rin_batch_PC10.txt")
+write.table(covOut.t, sep = "\t", quote = F, col.names = F, row.names = T, file = "INTERVAL_RNAseq_phase1-2_fullcovariates_foranalysis.txt")
