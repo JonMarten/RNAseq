e7c15be6247794e3632b19842e5bf4450b5d1c11
Jonathan Marten <jonmarten@gmail.com>
2020-07-23 16:58:56 +0100

    Scripts to run cis analysis

---
+++ 3_3a_map_cis_eQTLs_submissions_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --output=/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/logs/tensorqtl_cis_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+#SBATCH -J cis_eQTL
+#SBATCH -A PAUL-SL3-GPU
+#SBATCH --time=12:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks=4
+#SBATCH --gres=gpu:4
+#SBATCH -p pascal
+
+#! ############################################################
+. /etc/profile.d/modules.sh
+module purge
+module load rhel7/default-gpu
+module load miniconda3-4.5.4-gcc-5.4.0-hivczbz 
+module load cuda/9.2
+module load r-3.6.0-gcc-5.4.0-bzuuksv
+
+source activate tensorQTL
+
+CHR=$SLURM_ARRAY_TASK_ID 
+
+python 3_3b_map_cis_eQTLs.py $CHR
---
+++ 3_3b_map_cis_eQTLs.py
@@ -0,0 +1,41 @@
+# Python script to run cis-eQTL mapping. Chromosome is obtained from command line arguments
+
+import sys
+import pandas as pd
+import tensorqtl
+from tensorqtl import genotypeio, cis, trans
+
+path = "/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/"
+outpath = path + "results/cis-eQTLs"
+chr = str(sys.argv[1]) 
+
+phenotype_bed_file = path + "phenotypes/INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr" + chr + ".bed.gz"
+covariates_file = path + "covariates/INTERVAL_RNAseq_phase1-2_fullcovariates_foranalysis.txt"
+plink_prefix_path = path + "genotypes/INTERVAL_RNAseq_Phase1-2_imputed_b38_biallelic_MAF0.005_chr" + chr
+
+phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(phenotype_bed_file)
+covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T  # samples x covariates
+
+# Read in genotypes 
+pr = genotypeio.PlinkReader(plink_prefix_path)
+
+# load genotypes and variants into data frames
+genotype_df = pd.DataFrame(pr.load_genotypes(), index=pr.bim['snp'], columns=pr.fam['iid'])
+variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
+
+# Cis gene-level mapping
+cis_df = cis.map_cis(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
+tensorqtl.calculate_qvalues(cis_df, qvalue_lambda=0.85)
+cis_df.to_csv(outpath + "tensorqtl_cis_MAF0.005_cisPerGene_chr" + chr + ".csv", index=True, index_label = "Phenotype")
+
+# Cis nominal mapping
+cisnom_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix=outpath + "tensorqtl_cis_MAF0.005_cisNominal_chr" + chr)
+
+# Conditional analysis
+indep_df = cis.map_independent(genotype_df, variant_df, cis_df, phenotype_df, phenotype_pos_df, covariates_df)
+indep_df.to_csv(outpath + "tensorqtl_cis_MAF0.005_cisIndependent_chr" + chr + ".csv", index=True, index_label = "Phenotype")
+
+# GxE
+#interaction_s = pd.read_csv("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", sep = "\t", index_col=0, squeeze = True).T
+#cisGxE_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="Test_gxe", interaction_s=interaction_df)
+#cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="tensorqtl_cis_MAF0.005_cisGxE_chr1",interaction_s=interaction_s, maf_threshold_interaction=0.05,group_s=None, run_eigenmt=True, output_path=outpath)
