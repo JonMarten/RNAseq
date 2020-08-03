316f70d91a9fbba465fbf3c6442a96ac88a5b8e8
Jonathan Marten <JonMarten@users.noreply.github.com>
2020-08-03 10:51:34 +0100

    Index ChrX files

--- 3_2_index_bed.sh
+++ 3_2_index_bed.sh
@@ -9,7 +9,7 @@ cd /rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_ful
 bgzip INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed && tabix -p bed INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis.bed.gz
 
 # Compress and index per-chromosome files for conditional analysis
-for i in {1..22}
+for i in {1..23}
 do
   bgzip INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr${i}.bed && tabix -p bed INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr${i}.bed.gz
 done
