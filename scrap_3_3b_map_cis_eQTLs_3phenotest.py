e912e0396b34836951f9f42c012a0d6613f09248
Jonathan Marten <JonMarten@users.noreply.github.com>
2020-08-06 11:53:57 +0100

    Update conditional analysis testing

--- scrap_3_3b_map_cis_eQTLs_3phenotest.py
+++ scrap_3_3b_map_cis_eQTLs_3phenotest.py
@@ -6,8 +6,8 @@ import tensorqtl
 from tensorqtl import genotypeio, cis, trans
 
 path = "/home/jm2294/rds/rds-jmmh2-projects/interval_rna_seq/analysis/04_phase2_full_analysis/"
-outpath = path + "results/cis-eQTLs"
-chr = str(sys.argv[1]) 
+outpath = path + "results/cis_eQTLs/"
+chr = "21"
 
 phenotype_bed_file = path + "phenotypes/INTERVAL_RNAseq_phase1-2_filteredSamplesGenes_TMMNormalised_FPKM_Counts_foranalysis_chr" + chr + ".bed.gz"
 covariates_file = path + "covariates/INTERVAL_RNAseq_phase1-2_fullcovariates_foranalysis.txt"
@@ -15,33 +15,49 @@ plink_prefix_path = path + "genotypes/INTERVAL_RNAseq_Phase1-2_imputed_b38_biall
 
 all_phenotype_df, all_phenotype_pos_df = tensorqtl.read_phenotype_bed(phenotype_bed_file)
 
-# Limit to 3 phenotypes to test conditional analysis
-phenotype_df = all_phenotype_df[0:20]
-phenotype_pos_df = all_phenotype_pos_df[0:20]
-
-covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T  # samples x covariates
-
-# Read in genotypes 
-pr = genotypeio.PlinkReader(plink_prefix_path)
-
 # load genotypes and variants into data frames
 genotype_df = pd.DataFrame(pr.load_genotypes(), index=pr.bim['snp'], columns=pr.fam['iid'])
 variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
 
+covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T  # samples x covariates
+covariates_df = covariates_df[["age_RNA"]]
+# Read in genotypes 
+pr = genotypeio.PlinkReader(plink_prefix_path)
+
+
+# Limit to 3 phenotypes to test conditional analysis
+phenotype_df = all_phenotype_df[0:15]
+phenotype_pos_df = all_phenotype_pos_df[0:15]
+
 # Cis gene-level mapping
 cis_df = cis.map_cis(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
-#tensorqtl.calculate_qvalues(cis_df, qvalue_lambda=0.85) # this step crashes with such a small number of phenotypes, so just set q-values to p-values
-cis_df['qval'] = cis_df['pval_beta']
+tensorqtl.calculate_qvalues(cis_df, qvalue_lambda=0) # lambda of 0 is equivalent to BH correction
 cis_df.to_csv(outpath + "tensorqtl_cis_MAF0.005_cisPerGene_3phenotest_chr" + chr + ".csv", index=True, index_label = "Phenotype")
 
 # Cis nominal mapping
-cisnom_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix=outpath + "tensorqtl_cis_MAF0.005_cisNominal_3phenotest_chr" + chr)
+#cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix=outpath + "tensorqtl_cis_MAF0.005_cisNominal_3phenotest_chr" + chr)
+#cisnom_df = pd.read_parquet(outpath + "tensorqtl_cis_MAF0.005_cisNominal_3phenotest_chr21.cis_qtl_pairs.21.parquet")
 
 # Conditional analysis
 indep_df = cis.map_independent(genotype_df, variant_df, cis_df, phenotype_df, phenotype_pos_df, covariates_df, nperm=10000)
 indep_df.to_csv(outpath + "tensorqtl_cis_MAF0.005_cisIndependent_3phenotest_chr" + chr + ".csv", index=True, index_label = "Phenotype")
 
-# GxE
-#interaction_s = pd.read_csv("/rds/project/jmmh2/rds-jmmh2-projects/interval_rna_seq/analysis/03_tensorqtl/covariates/INTERVAL_RNAseq_phase1_GxE_neutPCT.txt", sep = "\t", index_col=0, squeeze = True).T
-#cisGxE_df = cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="Test_gxe", interaction_s=interaction_df)
-#cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df, prefix="tensorqtl_cis_MAF0.005_cisGxE_chr1",interaction_s=interaction_s, maf_threshold_interaction=0.05,group_s=None, run_eigenmt=True, output_path=outpath)
+
+
+
+# ENSG00000235609 on chr21 seems to break it
+phenotype_df = all_phenotype_df.loc[['ENSG00000235609','ENSG00000180530','ENSG00000155313']]
+phenotype_pos_df = all_phenotype_pos_df.loc[['ENSG00000235609','ENSG00000180530','ENSG00000155313']]
+cis_df = cis.map_cis(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
+cis_df['qval'] = cis_df['pval_beta']
+indep_df = cis.map_independent(genotype_df, variant_df, cis_df, phenotype_df, phenotype_pos_df, covariates_df, nperm=10000)
+
+phenotype_df = all_phenotype_df.loc[['ENSG00000235609','ENSG00000180530']]
+phenotype_pos_df = all_phenotype_pos_df.loc[['ENSG00000235609','ENSG00000180530']]
+cis_df = cis.map_cis(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covariates_df)
+cis_df['qval'] = cis_df['pval_beta']
+indep_df = cis.map_independent(genotype_df, variant_df, cis_df, phenotype_df, phenotype_pos_df, covariates_df, nperm=10000)
+
+
+phenotype_pos_df = phenotype_pos_df.loc['ENSG00000235609']
+
